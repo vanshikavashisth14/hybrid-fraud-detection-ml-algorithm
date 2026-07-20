d4d3f2e88d0ed0bda38604c353535d586874cd29
vanshikavashisth14 <VANSHIKAVASHISTH065@GMAIL.COM>
2026-07-20 16:42:21 +0530

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import IsolationForest
+from catboost import CatBoostClassifier
